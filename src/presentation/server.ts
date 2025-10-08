c0fc50d8172c85da23511322e0c6c248240e8f67
Franco Luciano Magnano <magnana441@gmail.com>
2025-10-08 20:33:09 +0200

    fin sección 8

---
+++ src/presentation/server.ts
@@ -0,0 +1,25 @@
+import { CheckService } from "../domain/use-cases/checks/check-service";
+import { CronService } from "./cron/cron-service";
+
+
+export class Server {
+
+    public static start() {
+
+        console.log('Server started...');
+
+        CronService.createJob(
+            '*/5 * * * * *',
+            () => {
+                const url = 'https://google.com';
+                // new CheckService().execute('https://www.google.com');
+                new CheckService(
+                    () => console.log(`${url} is success`),
+                    (error) => console.log(error)
+                ).execute(url);
+            }
+        );
+
+    }
+
+}
