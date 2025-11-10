2022870707672573d581946a34568717035f3272
Franco Luciano Magnano <magnana441@gmail.com>
2025-11-10 20:48:02 +0100

    Postgres and Mongo added as datasources

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,58 @@
 import {envs} from "./config/plugins/envs.plugin";
+import { LogModel, MongoDatabase } from "./data/mongo/index";
+import { PrismaClient } from "./generated/prisma/client";
 import { Server } from "./presentation/server";
 
 (async() => {
     main();
 })();
 
-function main() {
+async function main() {
+
+    await MongoDatabase.connect({
+        mongoUrl: envs.MONGO_URL,
+        dbName: envs.MONGO_DB_NAME
+    });
+
+    // * Crear una colección
+
+    // const newLog = await LogModel.create({
+    //     message: 'Test message desde Mongo',
+    //     origin: 'App.ts',
+    //     level: 'low',
+    // })
+
+    // await newLog.save();
+
+    // console.log(newLog);
+
+    // * Obtener entradas
+
+    // const logs = await LogModel.find();
+
+    // console.log(logs);
+
+    // * Prisma
+
+    const prisma = new PrismaClient();
+    // const newLog = await prisma.logModel.create({
+    //     data: {
+    //         level: 'MEDIUM',
+    //         message: 'Test message desde Prisma',
+    //         origin: 'App.ts',
+    //     }
+    // });
+
+    // console.log({newLog});
+
+    // const logs = await prisma.logModel.findMany({
+    //     where: {
+    //         level: 'MEDIUM',
+    //     }
+    // });
+
+    // console.log({logs});
+
     Server.start();
     
 }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,19 +1,29 @@
-import { CheckService } from "../domain/use-cases/checks/check-service"
+import { CheckServiceMultiple } from "../domain/use-cases/checks/check-service-mutiple"
 import { SendEmailLogs } from "../domain/use-cases/email/send-email-logs"
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource"
+import { MongoLogDatasource } from "../infrastructure/datasources/mongo-log.datasource"
+import { PostgresLogDatasource } from "../infrastructure/datasources/postgres-log.datasource"
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl"
 import { CronService } from "./cron/cron-service"
-import {EmailService} from './email/email.service'
+import { EmailService } from './email/email.service'
 
-const fileSystemLogRepository = new LogRepositoryImpl(
+const fslogRepository = new LogRepositoryImpl(
     new FileSystemDatasource()
+);
+
+const mongoLogRepository = new LogRepositoryImpl(
+    new MongoLogDatasource()
+);
+
+const postgresLogRepository = new LogRepositoryImpl(
+    new PostgresLogDatasource()
 );
 
 const emailService = new EmailService();
 
 export class Server {
 
-    public static start() {
+    public static async start() {
 
         console.log('Server started...');
 
@@ -32,21 +42,21 @@ export class Server {
 
         // * Mandar con archivos adjuntos
 
-        new SendEmailLogs(emailService, fileSystemLogRepository).execute('francolucianomagnano@hotmail.com')
+        // new SendEmailLogs(emailService, logRepository).execute('francolucianomagnano@hotmail.com')
         
         // * Ejemplo cron checkService
-        // CronService.createJob(
-        //     '*/5 * * * * *',
-        //     () => {
-        //         const url = 'https://www.google.com';
-        //         // new CheckService().execute('https://www.google.com');
-        //         new CheckService(
-        //             fileSystemLogRepository,
-        //             () => console.log(`${url} is success`),
-        //             (error) => console.log(error)
-        //         ).execute(url);
-        //     }
-        // );
+        CronService.createJob(
+            '*/5 * * * * *',
+            () => {
+                const url = 'https://www.google.com';
+                // new CheckService().execute('https://www.google.com');
+                new CheckServiceMultiple(
+                    [fslogRepository, mongoLogRepository, postgresLogRepository],
+                    () => console.log(`${url} is success`),
+                    (error) => console.log(error)
+                ).execute(url);
+            }
+        );
 
     }
 
